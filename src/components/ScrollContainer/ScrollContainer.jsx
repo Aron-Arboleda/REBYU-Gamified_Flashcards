686c27fb70bb3153f50e67bed0cea299479f2109
Aron-Arboleda <aronrez62@gmail.com>
2024-11-22 20:39:42 +0800

    Fixed really thick left and right border of scroll, figured out a trick for it

--- src/components/ScrollContainer/ScrollContainer.jsx
+++ src/components/ScrollContainer/ScrollContainer.jsx
@@ -1,7 +1,14 @@
 import "./ScrollContainer.css";
 
 const ScrollContainer = ({ children, height = "70vh", maxWidth = "600px" }) => {
-  return <div className="scrollContainer">{children}</div>;
+  return (
+    <>
+      <div className="parent-stackedDiv">
+        <div className="scrollContainer child-stackedDiv"></div>
+        <div className="child-stackedDiv content-stackedDiv">{children}</div>
+      </div>
+    </>
+  );
 };
 
 export default ScrollContainer;
