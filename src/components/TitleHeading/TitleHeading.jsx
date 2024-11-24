44bd9a18c5a1ab287667712377a9b89b66a2532c
Aron-Arboleda <aronrez62@gmail.com>
2024-11-24 12:19:15 +0800

    responsiveness of opened deck page done

--- src/components/TitleHeading/TitleHeading.jsx
+++ src/components/TitleHeading/TitleHeading.jsx
@@ -1,17 +1,7 @@
+import "./TitleHeading.css";
+
 const TitleHeading = ({ titleText }) => {
-  return (
-    <h1
-      style={{
-        fontFamily: "SG12",
-        fontSize: "3.5rem",
-        margin: "1rem 0rem",
-        color: "white",
-        WebkitTextStroke: "2px black",
-      }}
-    >
-      {titleText}
-    </h1>
-  );
+  return <h1 className="title-heading">{titleText}</h1>;
 };
 
 export default TitleHeading;
