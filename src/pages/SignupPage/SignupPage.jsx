67fee6f285b3bf98ea02450f649560be7aaee78f
Aron-Arboleda <aronrez62@gmail.com>
2024-12-06 19:37:14 +0800

    Fixed special character bug

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -25,7 +25,9 @@ const SignupPage = () => {
     const hasUpperCase = /[A-Z]/.test(password);
     const hasLowerCase = /[a-z]/.test(password);
     const hasNumber = /[0-9]/.test(password);
-    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    const hasSpecialChar = /[!@#$%^&*()\-_=+\[\]{};:'"\\|,.<>\/?`~]/.test(
+      password
+    );
 
     return (
       password.length >= minLength &&
